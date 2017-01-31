d1a734ed5989b5f02d83f81d45863e98916d7065
Aki-Tomoya <jingyee1900@live.cn>
2017-01-31 13:11:58 +0800

    Change the title of 404 page.

--- wp-content/themes/Mirrors-index/header.php
+++ wp-content/themes/Mirrors-index/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8"/>
-	<title><?php if (is_home()||is_search()) { echo "华南农业大学Linux镜像源 - SCAU Mirrors"; } else { wp_title(''); print " - "; bloginfo('name'); } ?> </title>
+	<title><?php if (is_home()||is_search()) { echo "华南农业大学Linux镜像源 - SCAU Mirrors"; } elseif (is_404()) {echo "哎呀，你找的东西不在这个次元喔"; print " - "; bloginfo('name'); } else { wp_title(''); print " - "; bloginfo('name'); } ?> </title>
 	<meta name="applicable-device" content="mobile">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
